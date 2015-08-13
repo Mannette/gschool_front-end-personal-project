8ad4fcf5caef0286ee58d4d86b0b5e733e090a2a
ethan <ethanmannette@gmail.com>
2015-08-13 09:38:19 -0600

    Better clarification for readme. Created boilerplate for questions.

--- js/utility.js
+++ js/utility.js
@@ -1,1 +1,76 @@
-// create deck
+// create question class
+function Questions(question, answers, rightAnswer) {
+  this.question = question;
+  this.answers = answers;
+  this.rightAnswer = rightAnswer;
+}
+
+// Create first question
+questionOne = new Question(
+  "",
+  [{1: "", 2: "", 3: "", 4: ""}],
+  ""
+);
+
+// Create second question
+questionTwo = new Question(
+  "",
+  [{1: "", 2: "", 3: "", 4: ""}],
+  ""
+);
+
+// Create third question
+questionThree = new Question(
+  "",
+  [{1: "", 2: "", 3: "", 4: ""}],
+  ""
+);
+
+// Create fourth question
+questionFour = new Question(
+  "",
+  [{1: "", 2: "", 3: "", 4: ""}],
+  ""
+);
+
+// Create fifth question
+questionFive = new Question(
+  "",
+  [{1: "", 2: "", 3: "", 4: ""}],
+  ""
+);
+
+// Create sixth question
+questionSix = new Question(
+  "",
+  [{1: "", 2: "", 3: "", 4: ""}],
+  ""
+);
+
+// Create seventh question
+questionSeven = new Question(
+  "",
+  [{1: "", 2: "", 3: "", 4: ""}],
+  ""
+);
+
+// Create eigth question
+questionEight = new Question(
+  "",
+  [{1: "", 2: "", 3: "", 4: ""}],
+  ""
+);
+
+// Create ninth question
+questionNine = new Question(
+  "",
+  [{1: "", 2: "", 3: "", 4: ""}],
+  ""
+);
+
+// Create tenth question
+questionTen = new Question(
+  "",
+  [{1: "", 2: "", 3: "", 4: ""}],
+  ""
+);
---
+++ readme.md
@@ -0,0 +1,38 @@
+# Mario Kart Quiz
+
+### Purpose
+
+Create a simple racing quiz game where the user must answer questions correctly in order to defeat their opponent in the race.
+
+Key features:
+
+1. Start and finish line.
+1. Questions answered correctly advance player.
+1. "Special" questions grant random items.
+1. Separate characters for player and opponent(s).
+1. Opponent(s) can answer questions incorrectly.
+
+### Technology
+
+* HTML
+* CSS
+* Javascript
+* Jquery
+
+### Implementation
+
+* OOP
+
+### Foreseen Difficulties
+
+* Creating animations for characters.
+* Allowing opponent to answer incorrectly.
+* Different effects for each item.
+
+### Stretch
+
+* Animations for attacks.
+* Different race tracks.
+* Multiple races.
+* Additional opponents who can attack each other.
+* Different difficulty levels.
