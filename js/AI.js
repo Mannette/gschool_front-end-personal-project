4bb2a626fb5a4743d6497711716afea01d2b2f1d
ethan <ethanmannette@gmail.com>
2015-08-14 16:02:47 -0600

    Added sound when character is selected. Made functions to add player and computer characters to track. Made additional functions to make player and computer move along track.

--- js/AI.js
+++ js/AI.js
@@ -1,18 +1,52 @@
-function ComputerPlayer(name) {
+var ComputerPlayer = function (name) {
+  this.name = name;
+};
 
-}
-
-var cpuOne = new ComputerPlayer(CPU1);
-var cpuTwo = new ComputerPlayer(CPU2);
-var cpuThree = new ComputerPlayer(CPU3);
+var cpuOne = new ComputerPlayer('cpu1');
+var cpuTwo = new ComputerPlayer('cpu2');
+var cpuThree = new ComputerPlayer('cpu3');
 
 // determine if computer player advances
-function computerAdvance() {
+// function computerAdvance () {
+//   var advance = Math.round(Math.random() * 3);
+//   if (advance !== 2) {
+//     // advance one block;
+//     // if (special === num) {
+//     //   // give computer player item
+//     // }
+//   }
+// }
+
+ComputerPlayer.prototype.computerAdvance = function () {
   var advance = Math.round(Math.random() * 3);
   if (advance !== 2) {
+
     // advance one block;
-    // if (special === num) {
-    //   // give computer player item
-    // }
+    computer = $('.computerCurrent').html();
+
+    console.log(computer);
+
+    // select index with character
+    var computerCurrent = $('.computerCurrent').index('.computer');
+
+    console.log(computerCurrent);
+// debugger;
+
+    // select next position
+    var nextPosition = computerCurrent + 1;
+
+    console.log(nextPosition);
+// debugger;
+
+    // remove character from current position
+    // playerCurrent.removeClass('playerCurrent');
+    $('.computerCurrent').html('');
+    $('#computerPosition').children().removeClass('computerCurrent');
+
+    // move character to next position
+    var test = $('#computerPosition').children()[nextPosition];
+    $(test).addClass('computerCurrent');
+    test.innerHTML = computer;
   }
-}
+  console.log(advance);
+};
--- js/dom.js
+++ js/dom.js
@@ -9,31 +9,137 @@ $(document).ready(function() {
   }
 
   var mario = '<img class="image" src="images/Mario.jpg">';
-  var luigi =
+  var luigi = '<img class="image" src="images/Luigi.jpg">';
 
+// --------------------- //
+// **SELECT CHARACTERS** //
+// --------------------- //
   // select mario
   $('#mario').click(function() {
     console.log('you selected mario!');
-    // place mario into playerStart
-    $('#playerStart').html('<img class="image" src="images/Mario.jpg">');
-    // place luigi into computerStart
-    $('#computerStart').html('<img class="image" src="images/Luigi.jpg">');
+    var player = mario;
+    var computer = luigi;
 
+    // place mario into playerStart
+    $('#playerStart').html(player).addClass('playerCurrent');
+
+    $('#playerPosition').css({
+      'background': 'red'
+    });
+
+    // place luigi into computerStart
+    $('#computerStart').html(computer).addClass('computerCurrent');
+
+    $('#computerPosition').css({
+      'background': 'green'
+    });
+
+
+    // hide character images
     $('#mario').hide();
     $('#luigi').hide();
+
+    // play sound
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', 'http://themushroomkingdom.net/sounds/wav/mk64/mk64_mario02.wav');
+    audioElement.setAttribute('autoplay', 'autoplay');
+
+    $.get();
+
+    audioElement.addEventListener('load', function() {
+      audioElement.Play();
+    }, true);
+
+    // Begin quiz.
+    // Append first question to question box.
+
+    // return player, computer;
 
   });
 
   // select luigi
   $('#luigi').click(function() {
     console.log('you selected luigi!');
-    // place mario into playerStart
-    $('#playerStart').html('<img class="image" src="images/Luigi.jpg">');
-    // place luigi into computerStart
-    $('#computerStart').html('<img class="image" src="images/Mario.jpg">');
+    var player = luigi;
+    var computer = mario;
 
+    // place luigi into playerStart
+    $('#playerStart').html(player).addClass('playerCurrent');
+
+    // $('#playerPositionOne').html(player).addClass('playerCurrent');
+
+    $('#playerPosition').css({
+      'background': 'green'
+    });
+
+    // place mario into computerStart
+    $('#computerStart').html(computer).addClass('computerCurrent');
+
+    $('#computerPosition').css({
+      'background': 'red'
+    });
+
+    // hide character images
     $('#mario').hide();
     $('#luigi').hide();
+
+    // play sound
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', 'http://themushroomkingdom.net/sounds/wav/mk64/mk64_luigi03.wav');
+    audioElement.setAttribute('autoplay', 'autoplay');
+
+    $.get();
+
+    audioElement.addEventListener('load', function() {
+      audioElement.Play();
+    }, true);
+
+    // Begin quiz.
+    // Append first question to question box.
+
+    // return player, computer;
+
+  });
+
+  // ----------------------- //
+  // ** TRACK ADVANCEMENT ** //
+  // ----------------------- //
+  // test advance button.
+  $('#advance').click(function() {
+
+    // computer advancing
+    cpuOne.computerAdvance();
+
+    // console.log('advance!');
+
+    player = $('.playerCurrent').html();
+
+    // console.log(player);
+
+    // select index with character
+    var playerCurrent = $('.playerCurrent').index('.player');
+
+    // console.log(playerCurrent);
+// debugger;
+
+    // select next position
+    var nextPosition = playerCurrent + 1;
+
+    // console.log(nextPosition);
+// debugger;
+
+    // remove character from current position
+    // playerCurrent.removeClass('playerCurrent');
+    $('.playerCurrent').html('');
+    $('#playerPosition').children().removeClass('playerCurrent');
+
+    // move character to next position
+    var test = $('#playerPosition').children()[nextPosition];
+    $(test).addClass('playerCurrent');
+    test.innerHTML = player;
+
+    // console.log(test);
+// debugger;
 
   });
 
